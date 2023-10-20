e605170a573a5c78f2804fda1c2cce47c1b1fec8
Joe Zhou <zhehui.zhou@outlook.com>
2023-10-20 19:26:26 -0400

    feat(example): ad server cache using bun

---
+++ ad-server-cache-bun/src/index.ts
@@ -0,0 +1,41 @@
+import {Elysia} from "elysia";
+import {Redis as Dragonfly} from 'ioredis';
+
+import {AdMetadataStore} from "./ads";
+import {AdMetadata, UserAdPreferences} from "./types";
+
+const client = new Dragonfly();
+
+const app = new Elysia()
+    .decorate("adMetadataCache", new AdMetadataStore(client))
+    .get(
+        "/ads/:userId",
+        async (context) => {
+            return await context.adMetadataCache.getAdMetadataListByUserPreference(context.params.userId);
+        },
+    )
+    .post(
+        "/ads",
+        async (context) => {
+            const metadata: AdMetadata = context.body;
+            await context.adMetadataCache.createAdMetadata(metadata);
+            context.set.status = 201;
+            return;
+        },
+        {body: AdMetadata}
+    )
+    .post(
+        "/ads/preferences",
+        async (context) => {
+            const userAdPreferences: UserAdPreferences = context.body;
+            await context.adMetadataCache.createUserPreference(userAdPreferences);
+            context.set.status = 201;
+            return;
+        },
+        {body: UserAdPreferences}
+    )
+    .listen(3000);
+
+console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
