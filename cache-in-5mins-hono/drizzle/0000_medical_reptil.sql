7427019f63f4b53b8b107294886ed1d1b2562f15
Joe Zhou <zhehui.zhou@outlook.com>
2025-07-07 17:33:33 -0400

    cache with hono example minor x02

--- cache-in-5mins-hono/drizzle/0000_medical_reptil.sql
+++ cache-in-5mins-hono/drizzle/0000_medical_reptil.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "short_links" (
 	"id" uuid PRIMARY KEY NOT NULL,
-	"original_url" varchar(2048) NOT NULL,
-	"short_code" varchar NOT NULL,
+	"original_url" varchar(4096) NOT NULL,
+	"short_code" varchar(30) NOT NULL,
 	"created_at" timestamp with time zone NOT NULL,
 	"expires_at" timestamp with time zone NOT NULL
 );
